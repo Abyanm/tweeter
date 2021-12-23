1428ba2698ab59d5235be8252353bcceda0ba598
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-12-23 05:04:33 +0000

    Completed responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,18 +3,24 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function(object) {
+
     return ` 
     <div class="tweet-container">
         <div>
             <i id="usert"class="fas fa-user-circle"></i>
-             <a class= "displayname">${object.user.name}</a>
-            <a id="username">${object.user.handle}</a>
+             <a class= "displayname">${escape(object.user.name)}</a>
+            <a id="username">${escape(object.user.handle)}</a>
         </div>
-        <div class="tweet"> ${object.content.text}
+        <div class="tweet"> ${escape(object.content.text)}
         </div>
-        <a class="time">${timeago.format(object.created_at)}</a>
+        <a class="time">${escape(timeago.format(object.created_at))}</a>
         <a class="icon-container">
          <i id="flag" class="far fa-flag"></i>
         <i id="retweet"class="fas fa-retweet"></i>
@@ -25,9 +31,9 @@ const createTweetElement = function(object) {
 }
 
 const renderTweets = function(tweets) {
-  for (const tweetInfo of tweets){
+  for (let i=tweets.length-1; i > 0; i--){
     // creates the html for each tweet
-    const $tweet = createTweetElement(tweetInfo)
+    const $tweet = createTweetElement(tweets[i])
 
     // adds the tweet-container element to tweets-container
     $('.tweets-container').append($tweet); 
@@ -58,15 +64,30 @@ $(document).ready(function() {
     event.preventDefault()
     let data = $( this ).serialize()
     let lengthOfTweet = data.length - 5
-    if(lengthOfTweet == 0 || lengthOfTweet > 140) {
-      return;
+    console.log(lengthOfTweet)
+    if(lengthOfTweet == 0 ) {
+      $("#error-zero").slideDown(1500)
+      setTimeout(()=>{
+        $("#error-zero").slideUp(1000)
+      },4000)
+      
+
+      // $("#errormessage").append("<div class='error'> This tweet is empty. Please add text. </div>")
+    } else if (lengthOfTweet > 140) {
+      $("#error-long").slideDown(1500)
+      setTimeout(()=>{
+        $("#error-long").slideUp(1000)
+      },4000)
     } else {
+
       // send data to localhost:8080/tweets
       $.ajax({
         type: "POST",
       url: "/tweets",
       data: $( this ).serialize()
       })
+      location.reload()
+
 
     }
 
